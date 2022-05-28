612358607f1db168193dbed901f0516fe99b1ff9
eadwigh <65357726+Hyzeen@users.noreply.github.com>
2022-05-28 03:01:08 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const Client = require('./structures/Client');
-require('dotenv').config();
-
-const client = new Client();
-client.login();
-
-process.on('uncaughtException', err => console.error(err.stack));
-process.on('unhandledRejection', err => console.error(err.stack));
